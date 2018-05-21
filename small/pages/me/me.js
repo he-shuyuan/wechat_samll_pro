a6cbb833a33657639be5ac22f1d3ac16b99f003c
linjituan <linjituan@foresee.com.cn>
2018-05-21 18:08:28 +0800

    commit cexiao

--- small/pages/me/me.js
+++ small/pages/me/me.js
@@ -4,13 +4,14 @@ Page({
         userInfo: {},
         hasUserInfo: false,
         canIUse: wx.canIUse('button.open-type.getUserInfo'),
-        ctx:null,
-        colorList: ['黑色','红色','黄色','蓝色','绿色'],
-        colorArray:['black','red','yellow','blue','green'],
-        currentColorIndex:0
+        ctx: null,
+        colorList: ['黑色', '红色', '黄色', '蓝色', '绿色'],
+        colorArray: ['black', 'red', 'yellow', 'blue', 'green'],
+        currentColorIndex: 0,
+        allDrawWorksPath: []
     },
     onLoad: function() {
-        console.log(app.globalData.userInfo)
+        console.log('e21', app.globalData.userInfo)
         if (app.globalData.userInfo) {
             this.setData({
                 userInfo: app.globalData.userInfo,
@@ -38,52 +39,109 @@ Page({
             })
         }
     },
-    onReady: function(op) { 
+    onReady: function(op) {
         this.setData({
-                ctx: wx.createContext()
-            })
+            ctx: wx.createCanvasContext('myCanvas')
+        })
         this.data.ctx.setLineWidth(1)
     },
     getUserInfo: function(e) {
-        console.log(e)
+        console.log('e22', e)
         app.globalData.userInfo = e.detail.userInfo
         this.setData({
             userInfo: e.detail.userInfo,
             hasUserInfo: true
         })
     },
-    _start:function(e){
-     
-      this.data.ctx.beginPath()
+    _start: function(e) {
+        console.log('ee')
+        this.data.ctx.beginPath()
 
-      this.data.ctx.moveTo(e.touches[0].x,e.touches[0].y);
-     
-      //   console.log(e.touches[0].x);
+        this.data.ctx.moveTo(e.touches[0].x, e.touches[0].y);
+        this.saveCurrentDrawWorks()
+            //   console.log(e.touches[0].x);
     },
-    _move:function(e){
-      this.data.ctx.lineTo(e.touches[0].x,e.touches[0].y);
-      this.data.ctx.stroke()
-      wx.drawCanvas({
-         canvasId: 'myCanvas',
-         reserve: true,
-         actions: this.data.ctx.getActions() // 获取绘图动作数组
-      })
-      this.data.ctx.moveTo(e.touches[0].x,e.touches[0].y);
+    _move: function(e) {
+        this.data.ctx.lineTo(e.touches[0].x, e.touches[0].y);
+        this.data.ctx.stroke()
+        wx.drawCanvas({
+            canvasId: 'myCanvas',
+            reserve: true,
+            actions: this.data.ctx.getActions() // 获取绘图动作数组
+        })
+        this.data.ctx.moveTo(e.touches[0].x, e.touches[0].y);
     },
-    _end:function(e){
-      //this.data.ctx.save();
-     console.log(this.data.ctx)
+    _end: function(e) {
+        //this.data.ctx.save();
+        console.log('e23', this.data.ctx)
     },
-    _backStep:function(e){
-      this.data.ctx.restore();
+    _backStep: function(e) {
+        this.data.ctx.restore();
     },
-    _sliderChange:function(e){
-      this.data.ctx.setLineWidth(e.detail.value);
+    _sliderChange: function(e) {
+        this.lineWidth = e.detail.value
+        this.data.ctx.setLineWidth(e.detail.value);
     },
-    _pickerChange:function(e){
-      this.data.ctx.setStrokeStyle(this.data.colorArray[e.detail.value])
-      this.setData({
-        currentColorIndex:e.detail.value
-      })
-    }
+    _pickerChange: function(e) {
+        this.paintColor = e.detail.value
+        this.data.ctx.setStrokeStyle(this.data.colorArray[e.detail.value])
+        this.setData({
+            currentColorIndex: e.detail.value
+        })
+    },
+    saveCurrentDrawWorks: function() {
+        console.log('232')
+        let self = this
+        wx.canvasToTempFilePath({
+            x: 0,
+            y: 0,
+            width: 0,
+            height: 0,
+            destWidth: 0,
+            destHeight: 0,
+            canvasId: 'myCanvas',
+            success: function(res) {
+                console.log('res1', res)
+                var imgPath = res.tempFilePath;
+                var allDrawWorksPath = self.data.allDrawWorksPath;
+                allDrawWorksPath.push(imgPath);
+                console.log('allDrawWorksPath', allDrawWorksPath)
+                self.setData({
+                        allDrawWorksPath: allDrawWorksPath,
+                    })
+                    // self.data.id++
+                console.log('allDrawWorksPath2', self.data.allDrawWorksPath)
+            },
+            fail: res => {
+                console.log('获取画布图片失败', res);
+
+            }
+        })
+    },
+    drawRevoke: function() {
+        console.log(123);
+        let self = this
+            // self.data.id--;
+        var allDrawWorksPath = self.data.allDrawWorksPath;
+        if (allDrawWorksPath == null || allDrawWorksPath.length == 0 || allDrawWorksPath == undefined) {
+            return;
+        }
+
+        var privWorksPath = allDrawWorksPath.pop();
+        console.log('privWorksPath', privWorksPath)
+        self.setData({
+            allDrawWorksPath: allDrawWorksPath,
+        })
+        console.log('allDrawWorksPath3', allDrawWorksPath)
+            // drawPath.pop();
+        self.data.ctx.setLineWidth(self.lineWidth);
+        // self.setPaintColor(self.data.StrokeStyle);
+        this.data.ctx.setStrokeStyle(this.data.colorArray[self.paintColor])
+        this.data.ctx.drawImage(privWorksPath, 0, 0, 300, 150);
+        this.data.ctx.draw();
+
+        if (allDrawWorksPath.length == 0) {
+
+        }
+    },
 })
